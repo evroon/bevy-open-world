9a337a5520499d4fdf3c036cd3e8b3c7cf6f2617
Erik Vroon <erik.vroon@channable.com>
2026-02-21 18:18:11 +0100

    Multiple tiles

--- crates/bevy-osm/src/location.rs
+++ crates/bevy-osm/src/location.rs
@@ -1,8 +1,6 @@
 use bevy::math::Vec2;
 use strum_macros::Display;
 
-use crate::chunk::{Chunk, get_chunk_for_coord};
-
 #[derive(PartialEq, Eq, Hash, Display, Clone)]
 pub enum Location {
     MonacoCenter,
@@ -10,14 +8,10 @@ pub enum Location {
 }
 
 impl Location {
-    pub fn get_chunk(&self) -> Chunk {
-        let center = match self {
+    pub fn get_world_center(&self) -> Vec2 {
+        match self {
             Self::MonacoCenter => Vec2::new(43.72264, 7.40848),
             Self::MonacoFull => Vec2::new(43.71795, 7.38732),
-        };
-        get_chunk_for_coord(center.x as f64, center.y as f64, 13)
-    }
-    pub fn get_world_center(&self) -> Vec2 {
-        self.get_chunk().get_lat_lon_area().center()
+        }
     }
 }
